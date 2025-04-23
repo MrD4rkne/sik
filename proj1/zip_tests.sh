10ea71baeda1f62c6421f2d17eceaa5852e5fb8e
Marcin Szopa <marcinsz.pv@gmail.com>
2025-04-23 09:04:15 +0200

    zal1: add script for ziping tests

---
+++ proj1/zip_tests.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+index="ms459531"
+replaced_index="ab12345"
+
+temp_dir="zip_tests"
+
+if ! rm -rf "$temp_dir"; then
+    echo "Error: Could not remove existing directory 'zip_tests'."
+    exit 1
+fi
+
+if ! mkdir "$temp_dir"; then
+    echo "Error: Could not create directory 'zip_tests'."
+    exit 1
+fi
+
+tests_dir="tests"
+
+mkdir "$temp_dir/$tests_dir"
+
+if ! $tests_dir/clean.sh; then
+    echo "Error: Cleanup script failed."
+    exit 1
+fi
+
+if ! cp -r "$tests_dir"/* "zip_tests/$tests_dir/"; then
+    echo "Error: Could not copy test files."
+    exit 1
+fi
+
+if ! cp "prepare.sh" "zip_tests/"; then
+    echo "Error: Could not copy prepare.sh."
+    exit 1
+fi
+
+if ! cp "TESTS_README.MD" "zip_tests/README.MD"; then
+    echo "Error: Could not copy TESTS_README.MD."
+    exit 1
+fi
+
+# Replace my initials with the given initials
+if ! find "$temp_dir" -type f -exec sed -i "s/$index/$replaced_index/g" {} \;; then
+    echo "Error: Could not replace initials in files."
+    exit 1
+fi
+
+tests_zip_name="tests.zip"
+
+if ! zip -r "$tests_zip_name" "$temp_dir"; then
+    echo "Error: Could not create zip archive."
+    exit 1
+fi
+
+if ! rm -rf "$temp_dir"; then
+    echo "Error: Could not remove temporary directory."
+    exit 1
+fi
+echo "Zip archive '$tests_zip_name' created successfully!"
